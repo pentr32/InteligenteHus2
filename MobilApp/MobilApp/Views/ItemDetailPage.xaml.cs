683bca6f39fe1c48dc73bd489bdc180c5f90ab50
Pent <robzaha@hotmail.com>
2021-04-21 11:00:50 +0200

    Add project files.

---
+++ MobilApp/MobilApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MobilApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MobilApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ MobilApp/MobilApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MobilApp.Models;
+using MobilApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobilApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
